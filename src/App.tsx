1052f4f434006f8034255ebfe05e18ed213af356
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2020-06-06 16:02:20 -0300

    criando a pagina home

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header'
+
+import Home from './pages/Home'
 
 const App = () => {
 
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick(){
-    setCounter(counter + 1);
-    console.log(counter)
-  }
 
   return (
-    <div>
-      <Header title="E-Coleta!" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-    </div>
+    <Home />
   );
 }
 
