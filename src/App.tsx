79df215560e0d3276e88c8483c6efbf5e5c0ae24
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2020-06-06 16:39:47 -0300

    configurando o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 const App = () => {
 
 
   return (
-    <Home />
+    <Routes />
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
         <main>
           <h1>Seu Marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-          <a href="/cadastro">
+          <a href="/create-point">
             <span>
               <FiLogIn />
             </span> 
